dc63cd0dffea90065e86034925e08083391212f8
Jeff <2607044640@qq.com>
2025-06-01 01:17:18 +0800

    solved compile error of #if

--- Source/MyRewardProject/Frameworks/MyHUD.cpp
+++ Source/MyRewardProject/Frameworks/MyHUD.cpp
@@ -3,6 +3,7 @@
 
 #include "MyHUD.h"
 
+#include "DataDrivenShaderPlatformInfo.h"
 #include "Kismet/GameplayStatics.h"
 #include "MyRewardProject/UMG/UMG_MainUI.h"
 
@@ -34,32 +35,33 @@ void AMyHUD::OnWindowFocusChanged(bool bIsFocused)
 	// FSlateApplication::Get().OnApplicationActivationStateChanged()
 	// .AddUObject(this, &AMyHUD::OnWindowFocusChanged);
 	// Don't pause in the editor, it's annoying
-#if !WITH_EDITOR
-	//In this area, There's no compilation check. Be careful not to add more logic.Otherwise, packaging will be failed
-	if ( bIsFocused )
+	if (GIsEditor)
 	{
-		// FString TempStr = FString::Printf(TEXT("Focus"));
-		// if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Red, TempStr, true, FVector2D(3, 3));
-		// UE_LOG(LogTemp, Error, TEXT("%s"), *TempStr);
+		//In this area, There's no compilation check. Be careful not to add more logic.Otherwise, packaging will be failed
+		if (bIsFocused)
+		{
+			// FString TempStr = FString::Printf(TEXT("Focus"));
+			// if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Red, TempStr, true, FVector2D(3, 3));
+			// UE_LOG(LogTemp, Error, TEXT("%s"), *TempStr);
 
-		// Unlimit game FPS
-		GEngine->SetMaxFPS( 0 );
+			// Unlimit game FPS
+			GEngine->SetMaxFPS(0);
 
-		// Unpause the game
-		UGameplayStatics::SetGamePaused(this,false);
+			// Unpause the game
+			UGameplayStatics::SetGamePaused(this, false);
+		}
+		else
+		{
+			// FString TempStr = FString::Printf(TEXT("NotFocus"));
+			// if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Red, TempStr, true, FVector2D(3, 3));
+			// UE_LOG(LogTemp, Error, TEXT("%s"), *TempStr);
+			// Reduce FPS to max 10 while in the background
+			GEngine->SetMaxFPS(10.0f);
+
+			// Pause the game, using your "show pause menu" function
+			UGameplayStatics::SetGamePaused(this, true);
+		}
 	}
-	else
-	{
-		// FString TempStr = FString::Printf(TEXT("NotFocus"));
-		// if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Red, TempStr, true, FVector2D(3, 3));
-		// UE_LOG(LogTemp, Error, TEXT("%s"), *TempStr);
-		// Reduce FPS to max 10 while in the background
-		GEngine->SetMaxFPS( 10.0f );
-
-		// Pause the game, using your "show pause menu" function
-		UGameplayStatics::SetGamePaused(this,true);
-	}
-#endif
 }
 
 bool AMyHUD::GetIsGameWindowFocused()
@@ -86,6 +88,6 @@ void AMyHUD::BeginPlay()
 
 	FSlateApplication::Get().OnApplicationActivationStateChanged()
 	                        .AddUObject(this, &AMyHUD::OnWindowFocusChanged);
-	
+
 	Super::BeginPlay();
 }
